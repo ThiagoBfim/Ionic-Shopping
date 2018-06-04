a37fda9e3ca3737a1d78609b02797bae40399502
Thiago Bomfim <thiagobomfim1995@gmail.com>
2018-06-04 09:02:48 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { RecipePage } from './../pages/recipe/recipe';
+import { RecipesPage } from './../pages/recipes/recipes';
+import { EditRecipePage } from './../pages/edit-recipe/edit-recipe';
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { ShoppingListPage } from '../pages/shopping-list/shopping-list';
+import { TabsPage } from '../pages/tabs/tabs';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    EditRecipePage,
+    RecipesPage,
+    RecipePage,
+    ShoppingListPage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    EditRecipePage,
+    RecipesPage,
+    RecipePage,
+    ShoppingListPage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
