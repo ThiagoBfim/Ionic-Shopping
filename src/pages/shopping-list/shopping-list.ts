df47dff80503bc846e01eb9330e20026d6d59c37
Thiago Bomfim <thiagobomfim1995@gmail.com>
2018-06-04 20:25:01 -0300

    Criando button on navBar to redirect page

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,19 +1,33 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ShoppingListService } from '../../service/shopping';
+import { Ingredient } from '../../models/Ingredient';
 @Component({
   selector: 'page-shopping-list',
   templateUrl: 'shopping-list.html',
 })
 export class ShoppingListPage {
 
+  listItens: Ingredient[];
 
-  constructor(public shoppingListService: ShoppingListService){}
+  constructor(public shoppingListService: ShoppingListService) { }
 
-  onAddItem(form: NgForm){
+  onAddItem(form: NgForm) {
     this.shoppingListService.addItem(form.value.ingredientName, form.value.ingredientQtd);
     form.reset();
+    this.loadItens();
   }
 
+  ionViewWillEnter() {
+    this.loadItens();
+  }
 
+  onRemoveItem(index: number){
+    this.shoppingListService.removeIten(index);
+    this.loadItens();
+  }
+
+  private loadItens() {
+    this.listItens = this.shoppingListService.retrieveAll();
+  }
 }
